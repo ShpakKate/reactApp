ebea81715fceecf06304790bed4c576dea86d6a8
Katsiaryna Shpak <kateshpakmom@gmail.com>
2025-09-03 14:13:21 +0300

    [2] Декомпозиция конфига. Опции конфигурации

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,23 @@
+import webpack from 'webpack';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildPlugins } from './buildPlugins';
+import { BuildOptions } from './type/config';
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode } = options;
+    return {
+        mode: mode,
+        entry: paths.entry,
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(options),
+    }
+}
