f4833b768afebab37c56c9ae619a6e35d8c377cd
Katsiaryna Shpak <kateshpakmom@gmail.com>
2025-09-05 13:48:44 +0300

    [8] classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
+    console.log('theme', theme)
     return (
-        <div className={`app ${theme}`}>
-            <button onClick={toggleTheme}> TOGGLE </button>
+        <div className={ classNames('app', {}, [ theme ]) }>
+            <button onClick={ toggleTheme }> TOGGLE </button>
             <Link to={'/'}> Главня страница </Link>
             <Link to={'/about'}> О сайте </Link>
             <Suspense fallback={<div>Loading...</div>}>
--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -5,13 +5,13 @@ const ThemeProvider: FC = ({children}) => {
     const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
     const [ theme, setTheme ] = useState<Theme>(defaultTheme);
     const defaultProps = useMemo(() => ({
-        theme,
-        setTheme,
-    }), [theme])
+        theme: theme,
+        setTheme: setTheme,
+    }), [ theme ])
 
     return (
-        <ThemeContext.Provider value={defaultProps}>
-            {children}
+        <ThemeContext.Provider value={ defaultProps }>
+            { children }
         </ThemeContext.Provider>
     );
 };
