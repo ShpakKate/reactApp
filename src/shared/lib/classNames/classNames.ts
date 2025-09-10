12c9a0e5788fdb01a59ed48bfda770bd3139a373
Katsiaryna Shpak <kateshpakmom@gmail.com>
2025-09-10 17:10:11 +0300

    [20] Тестовая среда. Настройка Jest. Добавлен тест

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,11 +2,11 @@ type Mods = Record<string, string | boolean>;
 
 export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
-        cls,
-        Object.entries(mods)
+        cls.trim(),
+        ...additional,
+        ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className]) => className),
-        ...additional,
-    ].join(' ');
+    ].join(' ').trim();
 }
 
