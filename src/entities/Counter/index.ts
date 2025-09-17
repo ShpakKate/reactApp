6270c18e9984f7776e1a930249a3592859db7601
Katsiaryna Shpak <kateshpakmom@gmail.com>
2025-09-17 09:48:06 +0300

    [30] Redux-toolkit. Entity. Тесты на всех уровнях МЕТКА

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,4 @@
+export { Counter } from './ui/Counter';
+export { counterActions } from './model/slice/counterSlice';
+export { counterReducer } from './model/slice/counterSlice';
+export type { CounterSchema } from './model/types/counterSchema';
---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,26 @@
+import { counterReducer, CounterSchema } from 'entities/Counter';
+import { counterActions } from 'entities/Counter/model/slice/counterSlice';
+
+describe('counterSlice.test', () => {
+    test('decrement', () => {
+        const state: CounterSchema = { value: 10 };
+
+        expect(
+            counterReducer(state as CounterSchema, counterActions.decrement),
+        ).toEqual({ value: 9 });
+    });
+
+    test('increment', () => {
+        const state: CounterSchema = { value: 10 };
+
+        expect(
+            counterReducer(state as CounterSchema, counterActions.increment),
+        ).toEqual({ value: 11 });
+    });
+
+    test('should work with empty state', () => {
+        expect(
+            counterReducer(undefined, counterActions.increment),
+        ).toEqual({ value: 1 });
+    });
+});
