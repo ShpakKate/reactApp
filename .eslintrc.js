0accf864bdc2c91210e124fa544c1705699b8c87
Katsiaryna Shpak <kateshpakmom@gmail.com>
2025-12-01 15:55:06 +0300

    89 Опции в линтере. Добавляем поддержку алисов в наш eslint плагин

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,7 +71,7 @@ module.exports = {
         'no-param-reassign': 'off',
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
-        '@kate_shpak_07/path-checker/import-checker': 'error',
+        '@kate_shpak_07/path-checker/import-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
