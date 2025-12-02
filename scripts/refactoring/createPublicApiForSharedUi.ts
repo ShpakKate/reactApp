d14ca802e17a37d0a240fec004ebe8f74b72dec6
Katsiaryna Shpak <kateshpakmom@gmail.com>
2025-12-02 12:45:36 +0300

    94 Shared UI public api. Автоматизация рефакторинга

---
+++ scripts/refactoring/createPublicApiForSharedUi.ts
@@ -0,0 +1,49 @@
+import { Project } from 'ts-morph';
+import path from 'path';
+
+const project = new Project();
+
+project.addSourceFilesAtPaths('src/**/*.ts');
+project.addSourceFilesAtPaths('src/**/*.tsx');
+
+const files = project.getSourceFiles();
+const uiPath = path.resolve(__dirname, '..', '..', 'src', 'shared', 'ui');
+const sharedUiDirectory = project.getDirectory(uiPath);
+const componentsDirs = sharedUiDirectory?.getDirectories();
+
+function isAbsolute(value: string): boolean {
+    const layers = ['app', 'shared', 'pages', 'widgets', 'entities', 'features'];
+
+    return layers.some((layer) => value.startsWith(layer));
+}
+
+componentsDirs?.forEach((directory) => {
+    const indexFilePath = `${directory.getPath()}/index.ts`;
+    const indexFile = directory.getSourceFile(indexFilePath);
+
+    if (!indexFile) {
+        const sourceCode = `export * from './${directory.getBaseName()}';\n`;
+        const file = directory.createSourceFile(indexFilePath, sourceCode, { overwrite: true });
+        file.save().then();
+    }
+});
+
+files.forEach((sourceFile) => {
+    const importDeclarations = sourceFile.getImportDeclarations();
+    importDeclarations.forEach((importDeclaration) => {
+        const value = importDeclaration.getModuleSpecifierValue();
+        const valueWithOutAlias = value.replace('@/', '');
+
+        const segments = valueWithOutAlias.split('/');
+
+        const isSharedLayer = segments[0] === 'shared';
+        const isUiSlice = segments[1] === 'ui';
+
+        if (isAbsolute(valueWithOutAlias) && isSharedLayer && isUiSlice) {
+            const result = valueWithOutAlias.split('/').slice(0, 3).join('/');
+            importDeclaration.setModuleSpecifier(`@/${result}`);
+        }
+    });
+});
+
+project.save();
